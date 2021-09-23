e2036d2a0144876c59f084c15746bafd21a9d206
AlexanderMaksimovich <maksimowwwicth@mail.ru>
2021-09-23 23:49:21 +0300

    create option --format

--- hexlet_code/gendiff/__init__.py
+++ hexlet_code/gendiff/__init__.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('fisrt_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format')
+
 
 def main():
     parser.parse_args()
