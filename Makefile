965aad6ba590475f09fb82b9c4339c2c7828ff0c
AlexanderMaksimovich <maksimowwwicth@mail.ru>
2021-09-24 11:31:10 +0300

    add genereate_diff function

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 reinstall-local:
 	poetry run python3 -m pip install --force-reinstall dist/*.whl
+
+reinstall:
+	python3 -m pip install --user --force-reinstall dist/*.whl
---
+++ gendiff/gendiff.py
@@ -0,0 +1,37 @@
+import json
+
+
+def generate_diff(file1, file2):
+    f1 = json.load(open(file1))
+    f2 = json.load(open(file2))
+    set_f1 = set(f1)
+    set_f2 = set(f2)
+
+    added = set_f2 - set_f1
+    deleted = set_f1 - set_f2
+    modified_and_unmodified = set_f1 & set_f2
+    modified = set()
+    unmodified = set()
+    for key in modified_and_unmodified:
+        if f1[key] == f2[key]:
+            unmodified.add(key)
+        else:
+            modified.add(key)
+
+    sorted_list_of_keys = sorted(list(set_f1 | set_f2))
+
+    result_string = ''
+    result_string += '{'
+    for key in sorted_list_of_keys:
+        if key in added:
+            result_string += f'\n  + {key}: {f2[key]}'
+        elif key in deleted:
+            result_string += f'\n  - {key}: {f1[key]}'
+        elif key in unmodified:
+            result_string += f'\n    {key}: {f1[key]}'
+        elif key in modified:
+            result_string += f'\n  - {key}: {f1[key]}'
+            result_string += f'\n  + {key}: {f2[key]}'
+    result_string += '\n}'
+
+    return result_string
